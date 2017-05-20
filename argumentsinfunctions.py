671d0b650f19b047f61b6eefb6d1a75fbc0a8719
anay-jain <anay9999682713@gmail.com>
2017-05-21 00:12:04 +0530

    added arguments in functions

---
+++ argumentsinfunctions.py
@@ -0,0 +1,15 @@
+#now taking arguments in  functions
+def ask_ok(prompt , retries = 4 , reminder="Please try again"): 
+	#Defining a functions with arguments and their default values
+	ok = input(promt) # taking input from user
+	if ok in ('Y' , 'y' , 'yes'):
+		return True
+	if ok in ('F' , 'f' , 'false'):
+		return False
+	retries = retries -1 
+	if retries < 0:
+		raise ValueError('Invalid user response')
+	print(reminder)		
+
+	# input(anay)
+	ask_ok('anay')	
